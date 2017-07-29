c4f7d51e1ad3141d4416c0ac9da208461ea70f1b
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-29 15:13:34 -0400

    adding jinja templates

--- headlines.py
+++ headlines.py
@@ -1,28 +1,21 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn' : 'http://rss.cnn.com/rss/edition.rss',
             'fox' : 'http://feeds.foxnews.com/foxnews/latest',
-            'iol' : 'http://www.iol.co.za/cmlink/1.640'}
+            'iol' : 'http://www.iol.co.za/cmlink/1.640',
+            'f1' : 'http://feeds.bbci.co.uk/sport/formula1/rss.xml?edition=uk'}
 
 @app.route("/")
 @app.route("/<publication>")
 def get_news(publication='bbc'):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-                <body>
-                    <h1> Headlines </h1>
-                    <b>{0}</b>
-                    <i>{1}</i>
-                    <p>{2}</p>
-                </body>
-            </html>""".format(first_article.get("title"), 
-                            first_article.get("published"), 
-                            first_article.get("summary"))
+    feed = feedparser.parse(RSS_FEEDS[publication.lower()])
+    #first_article = feed['entries'][0]   
+    return render_template("home.html", articles=feed['entries'])
+
 # run app    
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
