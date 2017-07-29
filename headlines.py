ea0ea6b1c51e174a964a4be716ed0bbc7a95c404
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-29 13:50:22 -0400

    rss feeds

--- headlines.py
+++ headlines.py
@@ -3,23 +3,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn' : 'http://rss.cnn.com/rss/edition.rss',
+            'fox' : 'http://feeds.foxnes.com/foxnews/latest',
+            'iol' : 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
-    first_article = feed['entries'][0]
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
 
+@app.route("/cnn")
+def cnn():
+    return get_news("cnn")
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
     return """<html>
-            <body>
-                <h1> BBC Headlines </h1>
-                <b>{0}</b>
-                <i>{1}</i>
-                <p>{2}</p>
-            </body>
+                <body>
+                    <h1> Headlines </h1>
+                    <b>{0}</b>
+                    <i>{1}</i>
+                    <p>{2}</p>
+                </body>
             </html>""".format(first_article.get("title"), 
                             first_article.get("published"), 
                             first_article.get("summary"))
-
+# run app    
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
