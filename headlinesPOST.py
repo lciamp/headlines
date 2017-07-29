8c46ad217c38ba6b28963d847532b34f2c543954
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-29 16:07:30 -0400

    using POST

---
+++ headlinesPOST.py
@@ -0,0 +1,26 @@
+import feedparser
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc' : 'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn' : 'http://rss.cnn.com/rss/edition.rss',
+            'f1' : 'http://feeds.bbci.co.uk/sport/formula1/rss.xml?edition=uk'}
+
+@app.route("/", methods=['GET', 'POST'])
+def get_news():
+    # get request from form, not the url
+    query = request.form.get("publication")
+    # check to see if the get is one of the rss feeds
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "f1"
+    else:
+        publication = query.lower()
+    # create the feed
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    ###first_article = feed['entries'][0]   
+    return render_template("home.html", articles=feed['entries'])
+
+# run app    
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
