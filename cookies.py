17be08cb109cf0ec40c86e990564a19a994d8dc0
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-30 00:04:27 -0400

    adding cookies

--- cookies.py
+++ cookies.py
@@ -17,25 +17,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # get headlines based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    # publications logic
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
     
-    # get weather based on user inpput or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    # weather logic
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
     
-    # get currency based on user input of default
-    currency_from = request.args.get('currency_from')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get('currency_to')
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    # currency logic
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
+    # get_rate returns a tuple (rate, dict(keys))
     rate, currencies = get_rate(currency_from, currency_to)
 
     # making the response object for the cookie
@@ -54,7 +47,7 @@ def home():
     response.set_cookie("city", city, expires=expires)
     response.set_cookie("currency_from", currency_from, expires=expires)
     response.set_cookie("currency_to", currency_to, expires=expires)
-    # return response object
+    # return response object (template)
     return response
     
 # function that gets the news
@@ -94,9 +87,24 @@ def get_rate(frm, to):
     to_rate = parsed.get(to.upper())
     return (("%.2f" % (to_rate/frm_rate)), parsed.keys())
 
+# fallback function that checks GETS->Cookies->DEFAULTS
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+
+
 # function that converts C to F
 def to_fahrenheit(tempature):
     return tempature * 1.8 + 32
+
+
+
+
+
 
 # run app    
 if __name__ == '__main__':
